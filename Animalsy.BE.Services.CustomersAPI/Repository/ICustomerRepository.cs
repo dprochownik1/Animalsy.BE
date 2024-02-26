6d589753393bed3fa1d8cbb07620f8c6bc1a6421
Dominik Prochownik <d.prochownik17@gmail.com>
2024-02-26 13:33:50 +0100

    Introduce ICustomerRepository

---
+++ Animalsy.BE.Services.CustomersAPI/Repository/ICustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using Animalsy.BE.Services.CustomersAPI.Models;
+
+namespace Animalsy.BE.Services.CustomersAPI.Repository
+{
+    public interface ICustomerRepository
+    {
+        Task<Guid> CreateAsync(Customer customer);
+        Task<IEnumerable<Customer>> GetAllAsync();
+        Task<Customer?> GetByIdAsync(Guid id);
+        Task<bool> TryUpdateAsync(Guid id, Customer customer);
+        Task<bool> TryDeleteAsync(Guid id);
+    }
+}
