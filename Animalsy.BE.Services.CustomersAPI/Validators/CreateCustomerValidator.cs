c6fb0c76a07367adc157a782acd86734fc71a124
Prochownik, Dominik <d.prochownik17@gmail.com>
2024-05-14 14:46:47 +0200

    Minor changes for validators

--- Animalsy.BE.Services.CustomersAPI/Validators/CreateCustomerValidator.cs
+++ Animalsy.BE.Services.CustomersAPI/Validators/CreateCustomerValidator.cs
@@ -7,11 +7,11 @@ namespace Animalsy.BE.Services.CustomersAPI.Validators
     {
         public CreateCustomerValidator(EmailValidator emailValidator, PhoneNumberValidator phoneNumberValidator)
         {
-            RuleFor(x => x.Name).NotEmpty().Length(1,20);
-            RuleFor(x => x.LastName).NotEmpty().Length(1,20);
-            RuleFor(x => x.City).NotEmpty().Length(1,20);
-            RuleFor(x => x.Street).NotEmpty().Length(1,40);
-            RuleFor(x => x.Building).NotEmpty().Length(1,20);
+            RuleFor(x => x.Name).NotEmpty().MaximumLength(20);
+            RuleFor(x => x.LastName).NotEmpty().MaximumLength(20);
+            RuleFor(x => x.City).NotEmpty().MaximumLength(20);
+            RuleFor(x => x.Street).NotEmpty().MaximumLength(40);
+            RuleFor(x => x.Building).NotEmpty().MaximumLength(20);
             RuleFor(x => x.PostalCode).NotEmpty().Length(6);
             RuleFor(x => x.Flat).Length(1,20).When(x => x.Flat is not null);
             RuleFor(x => x.EmailAddress).SetValidator(emailValidator);
