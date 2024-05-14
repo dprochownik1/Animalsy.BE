cd5d255b83ef2b406ca29324c95aeaddd280d1e3
Prochownik, Dominik <d.prochownik17@gmail.com>
2024-05-14 13:05:34 +0200

    Adding Db context changes

--- Animalsy.BE.Services.PetsAPI/Data/AppDbContext.cs
+++ Animalsy.BE.Services.PetsAPI/Data/AppDbContext.cs
@@ -1,14 +1,10 @@
-﻿using Animalsy.BE.Services.PetAPI.Models;
+﻿using Animalsy.BE.Services.PetsAPI.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Animalsy.BE.Services.PetsAPI.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options)
-        {
-        }
-
         public DbSet<Pet> Pets { get; set; }
     }
 }
